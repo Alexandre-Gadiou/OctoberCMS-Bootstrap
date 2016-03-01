c4eba40f683e50e4d33a3499c974eae192236dea
Alexandre Gadiou <alexandregadiou44@icloud.com>
2016-03-02 00:30:42 +0100

    code review

--- updates/builder_table_create_algad_bootstrap_events.php
+++ updates/builder_table_create_algad_bootstrap_events.php
@@ -1,10 +1,13 @@
-<?php namespace Algad\Bootstrap\Updates;
+<?php
+
+namespace Algad\Bootstrap\Updates;
 
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
 class BuilderTableCreateAlgadBootstrapEvents extends Migration
 {
+
     public function up()
     {
         Schema::create('algad_bootstrap_events', function($table)
@@ -12,13 +15,15 @@ class BuilderTableCreateAlgadBootstrapEvents extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id')->unsigned();
             $table->string('title')->nullable();
+            $table->string('slug')->nullable();
             $table->string('description')->nullable();
             $table->dateTime('date');
         });
     }
-    
+
     public function down()
     {
         Schema::dropIfExists('algad_bootstrap_events');
     }
+
 }
